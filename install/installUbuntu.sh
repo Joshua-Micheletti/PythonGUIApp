b179b4faec1e083a67882890e1175a84d20fb99b
Joshua-Micheletti <joshua.e.n.micheletti@gmail.com>
2022-09-10 22:12:19 +0200

    improved ubuntu install script

--- install/installUbuntu.sh
+++ install/installUbuntu.sh
@@ -5,5 +5,7 @@ sudo apt install python3-pip
 sudo apt install python-tk
 python3 -m pip install git+https://github.com/RedFantom/ttkthemes
 
-echo "#!/bin/bash" > launch.sh
-echo python3 app.py > launch.sh
+echo "#!/bin/bash" > ../launch.sh
+echo python3 app.py > ../launch.sh
+
+chmod +x ../launch.sh
